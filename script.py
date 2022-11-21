5a2c4cd2c0ec80673bba7e26f98ad0196c2b4d54
levathian1 <randomynot02@gmail.com>
2022-11-21 19:03:22 +0100

    added grayscale option

--- script.py
+++ script.py
@@ -1,8 +1,15 @@
 #if png convert image to rgb
-from PIL import Image
+from PIL import Image, ImageOps
 
 img = Image.open('cat.jpg').convert("RGB")
-nbColours = int(input("Enter the number of colours you would like to use "))
-result = img.convert('P', palette=Image.Palette.ADAPTIVE, colors=nbColours)
+
+grayscale = input("Grayscale or not (g or c)")
+
+if (grayscale == 'c'):
+    nbColours = int(input("Enter the number of colours you would like to use "))
+    result = img.convert('P', palette=Image.Palette.ADAPTIVE, colors=nbColours)
+else:
+    result = ImageOps.grayscale(img)
+    
 result.show()
 result.save("result.png")
