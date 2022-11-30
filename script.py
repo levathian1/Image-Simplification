0554415f55d5e5b4edfcff6ff831dd5a19e2cc38
levathian1 <randomynot02@gmail.com>
2022-11-30 14:21:32 +0100

    colour input number

--- script.py
+++ script.py
@@ -2,21 +2,14 @@
 from tkinter import *
 from PIL import Image, ImageOps
 
-
+nb_col = 5
 
 img = Image.open('cat.jpg').convert("RGB")
 
 
-def sel():
-    selected = "Vous avez sélectionné : " + v.get()
-    label.config(text = selected)
-    if (v.get() == 'c'):
-        #nbColours = int(input("Enter the number of colours you would like to use "))
-        result = img.convert('P', palette=Image.Palette.ADAPTIVE, colors=10)
-    elif(v.get() == 'g'):
-        print("hi")
-        result = ImageOps.grayscale(img)
-    return result
+
+
+
 
 def show_res():
     print("hello")
@@ -28,13 +21,32 @@ def show_res():
 root = Tk()
 
 v = StringVar()
+
+e1 = Entry(root)
+e1.insert("1", 5)
+label = Label(root)
+label.pack()
+
+def nb_colours():
+    return int(e1.get())
+
+def sel():
+    selected = "Vous avez sélectionné : " + v.get()
+    if (v.get() == 'c'):
+        #nbColours = int(input("Enter the number of colours you would like to use "))
+        result = img.convert('P', palette=Image.Palette.ADAPTIVE, colors=nb_colours())
+    elif(v.get() == 'g'):
+        print("hi")
+        result = ImageOps.grayscale(img)
+    return result
+
+
 v.set("") # initialiser
 r1 = Radiobutton(root, text="Color", variable=v, value="c", command=sel)
 r1.pack()
 r2 = Radiobutton(root, text="Greyscale", variable=v, value="g", command=sel)
 r2.pack()
-label = Label(root)
-label.pack()
+e1.pack()
 
 
 
